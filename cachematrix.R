2356802525861acd8c7cfc804a172e12def5f718
pikefish <galacticoopasnoste@gmail.com>
2014-07-25 13:09:40 +0200

    the file now contains my solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The function 'makeCacheMatrix' is absolutely the same as 'makeVector' from the example.
+## The  methods 'setmean' and 'getmean' are renamed to 'setinverse' and 'getinverse' since
+## they set and return the inverse of a matrix, respectively.
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(invr) m <<- invr
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## The fuction 'cacheSolve' is almost the same as 'cachemean' yet it calls the 'solve' function
+## instead of 'mean' to inverse the supplied matrix. It also checks whether the matrix is 
+## inveritble and returns a message if it is not so. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        if(!(nrow(data)==ncol(data)) || det(data) == 0) {
+                message("matirx is either non-square or singular")
+                invisible(m)
+        }
+        else {
+                m <- solve(data, ...)
+                x$setinverse(m)
+                m
+        }
 }
